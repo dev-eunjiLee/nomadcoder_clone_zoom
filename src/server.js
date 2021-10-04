82b8fc26bb9da71fc45c899fa08fd04287acf41d
dev.eunjilee <dev.eunji.lee@gmail.com>
2021-10-04 21:57:16 +0900

    0.3 FrontEnd Setup 10/04

--- src/server.js
+++ src/server.js
@@ -2,6 +2,16 @@ import express from "express";
 
 const app = express();
 
-console.log("hello");
+// * PUG 셋팅
+app.set('view engine', "pug"); // * 뷰 엔진 pug로 셋팅
+app.set("views", __dirname + "\\public\\view") // * view 템플릿 위치 셋팅
 
-app.listen(3000);
+// * static 작업
+app.use('/public', express.static(__dirname + "/public"))
+
+// * 라우터
+app.get("/", (req, res) => res.render("home")); // => view/home.pug로 간다.
+app.get("/*", (req, res) => res.redirect("/"));
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`)
+app.listen(3000, handleListen);
