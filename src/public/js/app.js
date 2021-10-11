bdbba1e0342c13ef2e0411589c924c7e143e5f69
dev.eunjilee <dev.eunji.lee@gmail.com>
2021-10-11 23:29:56 +0900

    prettier eslint

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,4 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+// * socket이 connection을 open 했을 때 발생
+socket.addEventListener("open", ()=>{console.log("Connected To Browser 🐱‍🏍")})
--- src/server.js
+++ src/server.js
@@ -1,3 +1,7 @@
+for (var i = 0; i < 4; i++) {
+  console.log("HELLO ESLINT");
+}
+
 import http from "http"; // * node.js에 내장되어 있기 때문에 따로 설치할 필요가 없다.
 import WebSocket from "ws";
 import express from "express";
@@ -6,11 +10,11 @@ import * as events from "events";
 const app = express();
 
 // * PUG 셋팅
-app.set('view engine', "pug"); // * 뷰 엔진 pug로 셋팅
-app.set("views", __dirname + "\\public\\view") // * view 템플릿 위치 셋팅
+app.set("view engine", "pug"); // * 뷰 엔진 pug로 셋팅
+app.set("views", __dirname + "\\public\\view"); // * view 템플릿 위치 셋팅
 
 // * static 작업 => 여기서는 템플릿에 등록된 자바스크립트 파일을 가져올 수 있도록 하기 위해서 셋팅
-app.use('/public', express.static(__dirname + "/public"))
+app.use("/public", express.static(__dirname + "/public"));
 
 // * 라우터
 app.get("/", (req, res) => res.render("home")); // => view/home.pug로 간다.
@@ -20,21 +24,18 @@ app.get("/*", (req, res) => res.redirect("/"));
 // const handleListen = () => console.log(`Listening on http://localhost:3000`)
 // app.listen(3000, handleListen);
 
-
 // * http 서버에 ws 서버 물리기 => 동일한 포트에서 http, ws request를 모두 처리할 수 있다. (웹소켓 서버만 만들어도 상관은 없다)
-const handleListen = () => console.log(`Listening on http or wss://localhost:3000`)
+const handleListen = () =>
+  console.log(`Listening on http or wss://localhost:3000`);
 const server = http.createServer(app); // * HTTP 서버
-const wss = new WebSocket.Server({server}) // * WEBSOCKET 서버
+const wss = new WebSocket.Server({ server }); // * WEBSOCKET 서버
 
 // * new Websocket.Server() 파라미터 중 server값에 대한 정보
 // * @param {(http.Server|https.Server)} [options.server] A pre-created HTTP/S server to use
 
-function handleConnection(socket){
-    console.log(socket) // * websocket 출력
-}
+wss.on("connection", (socket) => {
+  console.log("Connected To Browser 🐱‍🏍");
+  socket.send("hello");
+});
 
-wss.on("connection", handleConnection)
-
-server.listen(3000, handleListen)
-
-
+server.listen(3000, handleListen);
